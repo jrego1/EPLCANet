833684ae600ac6481c1af8f3f6bb7c7f6fb3e8fd
jrego1 <regoj15@gmail.com>
2024-09-25 08:50:19 -0700

    Reimplementation.

--- run/train_mnist_bp.sh
+++
@@ -1,1 +0,0 @@
-python main.py --model 'EPLCACNN' --task 'MNIST' --alg 'BPTT' --channels 32 32 64 --kernels 5 5 5 --paddings 0 0 0 --strides 1 1 1 --optim 'adam' --lrs 1e-4 1e-5 8e-6 --pools 'imm' --epochs 20 --act 'mysig' --todo 'train' --betas 0.0 0.1 --T1 30 --T2 10 --mbs 50 --device 3 --save --lca_lambda 0.25 --tau 200 --lca_iters 10 --softmax --loss cel --dict_loss 'combo' --seed 2
